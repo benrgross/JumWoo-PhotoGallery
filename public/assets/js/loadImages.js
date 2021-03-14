34d1d62f81adeaa092816c006637c346b1d98330
Ben Gross <benrgross@gmail.com>
2021-03-14 19:51:47 -0400

    lazy load

--- public/assets/js/loadImages.js
+++ public/assets/js/loadImages.js
@@ -32,12 +32,6 @@ function loadImages() {
     .then(() => initLazyImages());
 }
 
-function bucketApi() {
-  fetch("/api/images")
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-}
-
 function createCards(data) {
   const container = document.querySelector(".container");
   container.innerHTML = "";
@@ -70,77 +64,10 @@ function createCard(image) {
     "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPc9OX/TwAIpQOgE36vNQAAAABJRU5ErkJggg=="
   );
   img.setAttribute("data-src", image.image);
+
   img.setAttribute("alt", image.description);
   img.classList.add("lazy");
-
-  // const cardBody = createEl("div", "card-body");
-  // const ratingFormContainer = createEl(
-  //   "div",
-  //   "rating d-flex justify-content-start"
-  // );
-  // ratingFormContainer.setAttribute("data-id", image._id);
-  // ratingFormContainer.setAttribute("data-rating", image.rating);
-
-  // const ratingForm = createRatingForm(image);
-
-  // const cardText = createEl("p", "card-text font-weight-bold mt-2");
-
-  // cardText.innerText = `${image.description} (${image.rating})`;
-
-  // imageContainer.append(img);
-  // ratingFormContainer.append(ratingForm);
-  // cardBody.appendChild(ratingFormContainer);
-  // cardBody.appendChild(cardText);
   card.appendChild(img);
-  // card.appendChild(cardBody);
 
   return card;
-}
-
-function createRatingForm(image) {
-  const labelText = {
-    1: "One Star",
-    2: "Two Stars",
-    3: "Three Stars",
-    4: "Four Stars",
-    5: "Five Stars",
-  };
-
-  const form = createEl("form");
-  form.setAttribute("action", "post");
-
-  for (let i = 1; i <= 5; i++) {
-    const input = createEl("input", "visuallyhidden");
-    input.setAttribute("type", "radio");
-    input.setAttribute("name", "rating");
-    input.setAttribute("id", `${image._id}-star-${i}`);
-    input.setAttribute("value", i);
-
-    const label = createEl("label");
-    label.setAttribute("for", `${image._id}-star-${i}`);
-    const labelSpan = createEl("span", "visuallyhidden");
-    labelSpan.innerText = labelText[i];
-    const star = createEl("i", `fa-star ${image.rating >= i ? "fas" : "far"}`);
-
-    label.appendChild(labelSpan);
-    label.appendChild(star);
-    label.onclick = updateRating;
-    form.appendChild(input);
-    form.appendChild(label);
-  }
-
-  return form;
-}
-
-function updateRating(event) {
-  const [id, , rating] = event.currentTarget.getAttribute("for").split("-");
-  fetch(`/api/images/${id}`, {
-    method: "PUT",
-    body: JSON.stringify({ rating }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then(function () {
-    loadImages();
-  });
 }
